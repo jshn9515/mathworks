d2e2f5c20af7284518682c736d613ae84c5c8512
jshn9515 <jshn9510@gmail.com>
2025-08-21 12:21:49 -0400

    Add support for typst export

--- 2023-51MCM/Problem B/question1.py
+++ 2023-51MCM/Problem B/question1.py
@@ -51,8 +51,24 @@ plt.savefig('data/问题1-城市快递运输量网络图.svg')
 # For now, we use a static PageRank based on the total PCS
 rank = nx.pagerank(G, alpha=0.85, weight='PCS')
 
-df2 = pd.DataFrame.from_dict(dict(rank), orient='index', columns=['Score'])
-df2['Rank'] = df2.rank(method='max', ascending=False)
-df2.index.name = 'City'
+df2 = pd.DataFrame.from_dict(rank, orient='index', columns=['Score'])
+df2['Rank'] = df2.rank(method='max', ascending=False).astype(int)
+df2.reset_index(names='City', inplace=True)
+df2.set_index(keys='Rank', verify_integrity=True, inplace=True)
+df2['Income'] = [G.in_degree(city, weight='PCS') for city in df2.City]
+df2['Outcome'] = [G.out_degree(city, weight='PCS') for city in df2.City]
+df2['Pred'] = [G.in_degree(city) for city in df2.City]
+df2['Succ'] = [G.out_degree(city) for city in df2.City]
 df2.sort_index(inplace=True)
 df2.to_excel('data/问题1-城市快递运输量得分.xlsx', float_format='%.4f')
+
+TypstExport = True
+if TypstExport:
+    df3 = df2.head()
+    columns = ['城市', 'PageRank值', '收货量', '发货量', '上游路径数', '下游路径数']
+    df3.to_csv(
+        'data/问题1-城市快递运输量得分.csv',
+        float_format='%.4f',
+        index_label='排名',
+        header=columns,
+    )
